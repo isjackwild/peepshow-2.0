28da956c0ee8e2553895abc5c001cb65a63d1012
Jack Wild <jack.wild@live.co.uk>
2016-11-10 16:31:30 +0000

    browser not supported cover

--- app/client.js
+++ app/client.js
@@ -1,18 +1,30 @@
 // require('./vendor/face.js');
 // require('./vendor/ccv.js');
+require('./vendor/modernizr-custom.js');
 import { init } from './facial-recognition.js';
 
 const aboutButton = document.getElementsByClassName('about--open')[0];
 const closeButton = document.getElementsByClassName('about--close')[0];
 const options = [... document.getElementsByClassName('select__option')];
 const info = document.getElementsByClassName('info')[0];
+const aboutText = document.getElementsByClassName('about-text')[0];
 const woman = document.getElementsByClassName('woman')[0];
 const dress = document.getElementsByClassName('woman__dress')[0];
+// const notSupported = document.getElementsByClassName('not-supported')[0];
+// const wrapper = document.getElementsByClassName('wrapper')[0];
 let selectedOption = document.getElementsByClassName('select__option--selected')[0];
 
-init();
 
-requestAnimationFrame(() => {
+const kickIt = () => {
+	if (!Modernizr.getusermedia) {
+		aboutText.style.display = "none";
+		closeButton.innerHTML = "Sorry, your browser is not supported";
+	}
+
+	// wrapper.classList.add('wrapper--visible')
+
+	init();
+
 	aboutButton.addEventListener('click', () => {
 		info.classList.add('info--visible');
 	});
@@ -32,6 +44,10 @@ requestAnimationFrame(() => {
 			dress.style.backgroundImage = `url('/assets/images/dress--${girl}.png')`;
 		});
 	});
-});
+}
 
-
+if (document.addEventListener) {
+	document.addEventListener('DOMContentLoaded', kickIt);
+} else {
+	window.attachEvent('onload', kickIt);
+}
